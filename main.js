de107aec06c388a11c0773b50de5fe90a01bfac0
Dang Khoa <khoavodang1451997@gmail.com>
2022-03-28 20:32:08 +0700

    ghep code

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+
+
+// Tạo hàm pop up modal
 document.getElementById('shoppingCart').addEventListener('click', () => {
-    document.getElementById("")
+    const sideNav = document.getElementById("sideNav")
+    
+    sideNav.style.right = "0";
+});
+
+// Tạo hàm tắt modal
+
+window.addEventListener('click', () =>{
+    if (Event.target === sideNav) {
+        sideNav.style.right = "-100%";
+    };
 });
